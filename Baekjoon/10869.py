80e9f61ab0e64611916d1f0b07da31b10670d4d7
JIHO PARK <pjh5672.dev@gmail.com>
2021-03-26 00:19:27 +0900

    restore del files

---
+++ Baekjoon/10869.py
@@ -0,0 +1,2 @@
+A,B = input().split(' ')
+print(f'{int(A)+int(B)}\n{int(A)-int(B)}\n{int(A)*int(B)}\n{int(int(A)/int(B))}\n{int(A)%int(B)}')
---
+++ Baekjoon/1330.py
@@ -0,0 +1,5 @@
+A,B = input().split(' ')
+A,B = int(A), int(B)
+if A > B: print('>')
+elif A < B: print('<')
+else: print('==') 
