6ae258cf1835a4bb44351cc38063f6b1785426e5
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-04-11 08:40:20 +0900

    add:two-sum

---
+++ leetcode/1.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_map = {}
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target - num], i]
+            nums_map[num] = i
