73234a6b3d667df72280d0acec86a5fc1941bc54
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-04-13 08:45:51 +0900

    add:3sum

---
+++ leetcode/15.py
@@ -0,0 +1,32 @@
+# 3sum
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            left, right = i+1, len(nums)-1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == num[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return results
